9c319ccab4bc14c47f6cd2ff114aae67ee7ffb5f
irfanasandi <irfanasandi21@gmail.com>
2020-07-16 04:14:53 +0700

    done

---
+++ script.js
@@ -0,0 +1,5 @@
+const share = document.getElementById("share");
+const social = document.querySelector(".social");
+share.addEventListener("click", () => {
+  social.classList.toggle("active");
+});
